866f2d9033bb7184bae51614c01c8f9a123c78c8
danielmaangi <danielmaangi@gmail.com>
2023-09-24 19:19:47 +0300

    configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'KCM Oversight Data System (KODS)'
+copyright = '2023, Kenya Coordinating Mechanism'
+author = 'info@globalfundkcm.or.ke'
 
 release = '0.1'
 version = '0.1.0'
